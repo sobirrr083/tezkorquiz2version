cc6fa415cfc9d9923812aa6b084da8c7fd9f85c1
sobirrr083 <161307835+sobirrr083@users.noreply.github.com>
2025-12-30 13:01:06 +0500

    Update protect.js

--- protect.js
+++ protect.js
@@ -4,8 +4,12 @@ import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/
 const firebaseConfig = {
     apiKey: "AIzaSyBxIpLY5WasiSIlDL4japBLnnXRwOLUKEQ",
     authDomain: "tezkor-quiz-bbe3c.firebaseapp.com",
-    // ... boshqa sozlamalar bo‘lsa qo‘ying
+    projectId: "tezkor-quiz-bbe3c",
+    storageBucket: "tezkor-quiz-bbe3c.firebasestorage.app",
+    messagingSenderId: "143161522496",
+    appId: "1:143161522496:web:252108ace3a4c19f35e2a5"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
